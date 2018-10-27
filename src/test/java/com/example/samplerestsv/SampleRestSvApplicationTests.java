3c0e05c370863bc83fd20db66239abd326245637
btishinoyu <ishinoyu@pm.nttdata.co.jp>
2018-10-27 14:57:44 +0900

    modified test.

--- src/test/java/com/example/samplerestsv/SampleRestSvApplicationTests.java
+++ src/test/java/com/example/samplerestsv/SampleRestSvApplicationTests.java
@@ -7,9 +7,12 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,19 +26,37 @@ public class SampleRestSvApplicationTests {
 	@Autowired
 	private RestTemplateBuilder restTemplateBuilder;
 	
+	@Value("${host.address}")
+	private String hostAddr;
+	
 	private RestOperations restOperations;
 
-	private static final String BASE_URI = "http://localhost:8080/api/sample/";
+	private String baseUrl;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		SpringApplication.run(SampleRestSvApplication.class, new String[] {});
+	}
 
 	@Before
 	public void setUp() {
 		restOperations = restTemplateBuilder.build();
+		baseUrl = "http://" + hostAddr + "/api/sample/";
+	}
+	
+	@Test
+	public void testHello() throws URISyntaxException {
+		URI url = new URI(baseUrl + "hello");
+		
+		SampleResource sr = restOperations.getForObject(url, SampleResource.class);
+
+		assertThat(sr.getMessage(), is("hello world"));
 	}
 
 	@Test
-	public void contextLoads() throws URISyntaxException {
+	public void testUser() throws URISyntaxException {
 
-		URI url = new URI(BASE_URI + "user/1");
+		URI url = new URI(baseUrl + "user/1");
 
 		SampleResource sr = restOperations.getForObject(url, SampleResource.class);
 
